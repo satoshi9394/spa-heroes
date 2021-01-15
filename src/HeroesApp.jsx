d9854aa6fb053232ccf54d5d04eb419da048e9d6
Angel Santillan <angelsb94@gmail.com>
2021-01-14 19:34:43 -0600

    router tradicional

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import AppRouter from './routers/AppRouter'
 
 const HeroesApp = () => {
   return (
-    <div>
-      <h1>HeroesApp</h1>
-    </div>
+    <>
+      <AppRouter/>
+    </>
   )
 }
 
---
+++ src/components/heroes/Heroes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Heroes = () => {
+  return (
+    <div>
+      <h1>Heroe Screen</h1>
+    </div>
+  )
+}
+
+export default Heroes
