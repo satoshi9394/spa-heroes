894273a2f0a44f1c8499237ed11bb8b568e5a66e
Angel Santillan <angelsb94@gmail.com>
2021-01-14 21:43:48 -0600

    mejorando la logica de la vista descriptiva del heroe

--- src/components/heroes/HeroesScreen.jsx
+++ src/components/heroes/HeroesScreen.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 import { Redirect, useParams } from 'react-router-dom'
 import { getHeroeById } from '../../selectors/getHeroesById';
 
-const HeroesScreen = () => {
+const HeroesScreen = ({ history }) => {
 
   const { heroeId } = useParams();
   const hero = getHeroeById(heroeId)
   if ( !hero ) {
     return <Redirect to="/" />
+  }
+  const handleReturn = () => {
+    if( history.length <= 2) {
+      history.push('/');
+    } else {
+      history.goBack()
+    } 
   }
   const {
     superhero,
@@ -18,8 +25,30 @@ const HeroesScreen = () => {
   } = hero;
 
   return (
-    <div>
-      <h1>Heroe Screen</h1>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img
+          src={`../assets/heroes/${ heroeId }.jpg`}
+          className="img-thumbnail"
+          alt={superhero}
+        />
+      </div>
+      <div className="col-8">
+        <h3>{ superhero }</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item"> <b> Alter ego: </b>{ alter_ego } </li>
+          <li className="list-group-item"> <b> Publisher: </b>{ publisher } </li>
+          <li className="list-group-item"> <b> Fisrt appearence: </b>{ fisrt_appearence } </li>
+        </ul>
+        <h5>Characters</h5>
+        <p>{characters}</p>
+        <button 
+          className="btn btn-outline-info"
+          onClick={ handleReturn }
+        >
+          Return
+        </button>
+      </div>
     </div>
   )
 }
